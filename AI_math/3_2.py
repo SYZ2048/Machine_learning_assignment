7283c02622da1d54c9589a3868ef42206b645a00
SYZ2048 <19307130359@fudan.edu.cn>
2023-11-07 15:54:04 +0800

    update for 最速下降法（梯度下降法）

--- AI_math/3_2.py
+++ AI_math/3_2.py
@@ -2,8 +2,8 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 # 给定的矩阵 A 和向量 b
-A = np.array([[3, 2, 2], [2, 2, 1], [2, 1, 3]])
-# A = np.array([[3, 2, 1], [2, 3, 1], [1, 1, 2]])
+# A = np.array([[3, 2, 2], [2, 2, 1], [2, 1, 3]])
+A = np.array([[3, 2, 1], [2, 3, 1], [1, 1, 2]])
 b = np.array([1, 2, 3])
 
 # 计算最优解 x_opt np.linalg.inv: A^-1
@@ -15,19 +15,19 @@ x0 = np.array([0, 0, 0])
 # 步长 alpha，容差 tol 和最大迭代次数
 alpha = 0.05  # 学习率或迭代步长，这个值可能需要调整以确保迭代的稳定性
 tolerance = 1e-6
-max_iterations = 50  # 最大迭代次数
+max_iterations = 100  # 最大迭代次数
 f_xk = []
 dist_xk_sub_xopt = []
 
 
 # 目标函数
-def f(x, A, b):
-    return x.T @ A @ x - b.T @ x
+def f(vec_x, matrix, vec_b):
+    return vec_x.T @ matrix @ vec_x - vec_b.T @ vec_x
 
 
 # 目标函数的梯度
-def grad_f(x, A, b):
-    return 2 * A @ x - b
+def grad_f(vec_x, matrix, vec_b):
+    return 2 * matrix @ vec_x - vec_b
 
 
 # 最速下降法（梯度下降法）
@@ -39,8 +39,9 @@ def gradient_descent(A, b, x0, alpha, tol, max_iterations):
             break
         r_i = b - A @ x
         d_i = -gradient
-        alpha_i = (d_i.T @ r_i) / (d_i.T @ A @ d_i)
+        alpha_i = (b.T @ d_i - 2 * x.T @ A @ d_i) / (2 * d_i.T @ A @ d_i)
         x = x + alpha_i * d_i  # 更新 x
+        # x = x + alpha * d_i
         # 计算当前解与最优解的距离
         distance = np.linalg.norm(x - x_opt)
 
@@ -57,10 +58,12 @@ fig = plt.figure()
 ax1 = fig.add_subplot(1, 2, 1)
 ax1.plot(f_xk)
 ax1.set_ylabel('f(xk)')
+ax1.set_xlabel('Iteration')
 
 ax2 = fig.add_subplot(1, 2, 2)
 ax2.plot(dist_xk_sub_xopt)
 ax2.set_ylabel('|xk-x_opt|')
+ax2.set_xlabel('Iteration')
 plt.tight_layout()  # 自动调整子图参数，使之填充整个图像区域。
 plt.show()
 
